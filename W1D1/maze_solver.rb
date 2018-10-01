561a92a3082fa2778e46531b0b68e78c1d830690
Ammar <ammar.alnatsheh@gmail.com>
2018-09-30 22:34:27 -0700

    finish project for w1d1

--- W1D1/maze_solver.rb
+++ W1D1/maze_solver.rb
@@ -1,10 +1,13 @@
+require 'byebug'
 class Maze_Solver
+
+  attr_reader :path, :start_idx, :end_idx, :maze
 
   def initialize(filename)
     @maze = load_map(filename)
     @start_idx = find_char("S")
     @end_idx = find_char("E")
-    @path = find_path
+    @path = find_path(@start_idx,@end_idx,@maze)
   end
 
   def load_map(filename)
@@ -24,7 +27,100 @@ class Maze_Solver
     end
   end
 
-  def find_path
+  def find_path(start_idx , end_idx, grid)
+
+    if ( start_idx == end_idx )
+      return [start_idx[0] , start_idx[1]]
+    end
+
+    #1
+    x = start_idx[0] - 1
+    y = start_idx[1]
+    if ( valid_position( x , y , grid) )
+      grid[x][y] = "X"
+      path = find_path(x , y, grid)
+      unless ( path.nil? )
+        return [[x,y]] + path
+      end
+    end
+
+    #2
+    x = start_idx[0] + 1
+    y = start_idx[1]
+    if ( valid_position( x , y , grid) )
+      grid[x][y] = "X"
+      path = find_path(x , y, grid)
+      unless ( path.nil? )
+        return [[x,y]] + path
+      end
+    end
+    #3
+    x = start_idx[0]
+    y = start_idx[1] - 1
+    if ( valid_position( x , y , grid) )
+      grid[x][y] = "X"
+      path = find_path(x , y, grid)
+      unless ( path.nil? )
+        return [[x,y]] + path
+      end
+    end
+    #4
+    x = start_idx[0]
+    y = start_idx[1] + 1
+    if ( valid_position( x , y , grid) )
+      grid[x][y] = "X"
+      path = find_path(x , y, grid)
+      unless ( path.nil? )
+        return [[x,y]] + path
+      end
+    end
+    #5
+    x = start_idx[0] + 1
+    y = start_idx[1] + 1
+    if ( valid_position( x , y , grid) )
+      grid[x][y] = "X"
+      path = find_path(x , y, grid)
+      unless ( path.nil? )
+        return [[x,y]] + path
+      end
+    end
+    #6
+    x = start_idx[0] - 1
+    y = start_idx[1] - 1
+    if ( valid_position( x , y , grid) )
+      grid[x][y] = "X"
+      path = find_path(x , y, grid)
+      unless ( path.nil? )
+        return [[x,y]] + path
+      end
+    end
+    #7
+    x = start_idx[0] - 1
+    y = start_idx[1] + 1
+    if ( valid_position( x , y , grid) )
+      grid[x][y] = "X"
+      path = find_path(x , y, grid)
+      unless ( path.nil? )
+        return [[x,y]] + path
+      end
+    end
+    #8
+    x = start_idx[0] + 1
+    y = start_idx[1] - 1
+    if ( valid_position( x , y , grid) )
+      grid[x][y] = "X"
+      path = find_path(x , y, grid)
+      unless ( path.nil? )
+        return [[x,y]] + path
+      end
+    end
+
+
+  end
+
+  def valid_position( x , y , grid)
+    #make sure the x,y posion in the grid borders and its empty cell
+    x > -1 && y > -1 && x < grid[0].length && y < grid.length && grid[x][y] == " "
   end
 
 end
